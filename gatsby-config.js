02f2efdaa71f5b5ea34485fa25fbc797b8c1c1e2
Doug Leinen <doug@geletkaplus.com>
2022-10-21 16:19:03 -0500

    init git commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,25 @@
+// const linkResolver = require("./example-route-to-linkResolver");
+
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Prismic Test`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
-}
+  plugins: [
+    // ...
+    "gatsby-plugin-image",
+    {
+      resolve: "gatsby-source-prismic",
+      options: {
+        repositoryName: process.env.GATSBY_PRISMIC_REPO_NAME,
+        accessToken: process.env.PRISMIC_ACCESS_TOKEN,
+        customTypesApiToken: process.env.PRISMIC_CUSTOM_TYPES_API_TOKEN,
+        // linkResolver: (doc) => linkResolver(doc),
+      },
+    },
+  ],
+};
